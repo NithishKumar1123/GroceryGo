26afc4f1fdfe6a1d8a264851a3932b70d4798fcc
Nithish Kumar <98147654+NithishKumar1123@users.noreply.github.com>
2024-04-21 00:57:27 +0530

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+echo "Welcome to the setup." 
+echo "This will activate the local virtual env and run the required python files."
+echo "You can rerun this without any issues."
+
+if [ -d ".env" ];
+then
+    echo "Enabling virtual env"
+else
+    echo "No virtual env. Please run 'local_setup.sh' first"
+    exit N
+fi
+
+# Activate virtual env
+source venv/bin/activate
+python app.py
+deactivate
